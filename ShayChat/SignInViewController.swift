aa3f5c866db4288dfc057fd7a010ce26a1142b17
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-25 17:40:08 -0400

    Added Firebase and UI for signing page

---
+++ ShayChat/SignInViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignInViewController.swift
+//  ShayChat
+//
+//  Created by Merritte on 10/25/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func startChattingTapped(_ sender: AnyObject) {
+    }
+
+
+}
+
