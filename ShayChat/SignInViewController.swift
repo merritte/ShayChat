12b170d3977a9ee977fd81438e4ffda044db547c
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-26 16:15:46 -0400

    added Firebase pods for auth and data also started adding code for user sign in

--- ShayChat/SignInViewController.swift
+++ ShayChat/SignInViewController.swift
@@ -7,21 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
     @IBAction func startChattingTapped(_ sender: AnyObject) {
+        
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("------------Hey, we have an error:\(error)-------------")
+            } else {
+                print("Signed in successfully")
+            }
+        })
+        
     }
-
-
+    
 }
 
