6fb8eb2f065f20f742c622e1fd7c8dd1152e083b
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-27 22:35:58 -0400

    created pictureViewController and created Picture UI

---
+++ ShayChat/PictureViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PictureViewController.swift
+//  ShayChat
+//
+//  Created by Merritte on 10/27/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func nextTapped(_ sender: AnyObject) {
+    }
+    
+}
