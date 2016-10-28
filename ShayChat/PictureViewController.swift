afcb7d2b4c6541b63f567b6bf9d44c906d0b8f48
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-28 18:32:33 -0400

    added ability to access photo library

--- ShayChat/PictureViewController.swift
+++ ShayChat/PictureViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -17,15 +17,37 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     
+    var imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
   
-    @IBAction func cameraTapped(_ sender: AnyObject) {
+    @IBAction func cameraTapped(_ sender:AnyObject) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: AnyObject) {
