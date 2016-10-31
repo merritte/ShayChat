221df98af812b25ee2c604b22e91b395595a19f6
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-31 17:28:44 -0400

    added ability to pull users from firebase to send snaps to

--- ShayChat/SelectUserViewController.swift
+++ ShayChat/SelectUserViewController.swift
@@ -7,28 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var users : [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            
+            
+            
+            let user = User()
+            user.email = (snapshot.value as! NSDictionary)["email"] as! String
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            
+            self.tableView.reloadData()
+            
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        return cell
     }
-
-  
-
+    
+    
+    
 }
---
+++ ShayChat/user.swift
@@ -0,0 +1,14 @@
+//
+//  user.swift
+//  ShayChat
+//
+//  Created by Merritte on 10/31/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = " "
+    var uid = " "
+}
