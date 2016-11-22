87af107792f828a78577064d28881223004b28ee
Shay-Jahen Merritte <Shay@Merritte.com>
2016-11-21 22:47:32 -0500

    snaps pull down from firebase into table

---
+++ ShayChat/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  ShayChat
+//
+//  Created by Merritte on 11/21/16.
+//  Copyright © 2016 Merritte. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var descrip = ""
+    var from = " "
+}
